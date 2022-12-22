aab01ac82175f512cb940d2f06d418736d3e6d36
aleksanrabernadka <106868378+aleksanrabernadka@users.noreply.github.com>
2022-12-22 13:04:15 +0100

    added link on gitHub

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ function App() {
         <h1>Weather App</h1>
 
         <SearchForm />
+        <footer>
+          <a
+            href="https://github.com/aleksanrabernadka/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced {}
+          </a>
+          on Github
+        </footer>
       </header>
     </div>
   );
